ad1aba5f61e57fc2d96c478bc33c0ff06748dafe
xdjcl <xdjcl@outlook.com>
2016-04-11 17:23:35 +0800

    Dynamic Time Warping (DTW)的matlab实现
    DTW是一种计算两个序列之间距离的方法，上面是相关MATLAB代码。其中文件夹dynamic_time_warping_v2.1中的代码是参见Quan
    Wang, Signal Analysis and Machine Perception Laboratory的。

---
+++ dtw.m
@@ -0,0 +1,43 @@
+% Jingchang Liu 2016/04/11
+% Dynamic Time Warping
+% get the distance of two sequences
+
+function d  = dtw(Q,C,w)
+    % Q,C: two sequences
+    % w: window parameter
+    %      if Q(i) is matched with C(j) then |i-j|<=w
+    % d: resulting distance
+
+    if nargin<3
+        w=Inf;
+    end
+
+    %% initialize DTW
+    % DTW(i+1,j+1): The accumulated distance between
+    %   Q(1) to Q(i) and C(1) to C(j)
+    % M: the length of sequence Q
+    % N: the length of sequence C
+    M = length(Q);
+    N = length(C);
+
+    DTW = zeros(M+1,N+1);
+    w = max([w,abs(M-N)]);
+
+    for i = 2:M+1
+        for j =2:N+1
+            DTW(i,j) = inf;
+        end
+    end
+    DTW(1,1) = 0;
+
+    %% DTW algorithm
+    % d(i,j):The distance between Q(i) and C(j)
+    for i = 2:M+1
+        for j = max([2,i-w+1]) : min([N+1,i+w+1])
+            d = norm(Q(i-1)-C(j-1));
+            DTW(i,j) = d+ min([DTW(i-1,j),DTW(i,j-1),DTW(i-1,j-1)]);
+        end
+    end
+
+    d = DTW(M+1,N+1)
+end
